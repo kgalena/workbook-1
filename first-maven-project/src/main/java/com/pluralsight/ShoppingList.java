f45901821db6c68f4fbd007ce5e4393552e3035b
Kevin Galeana <kevinbgaleana@gmail.com>
2025-04-10 10:27:31 -0400

    completed shopping list

---
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("apples,water,toothpaste,socks,watermelon,boxers,shirts,shoes,snacks,grapes");
+    }
+}
