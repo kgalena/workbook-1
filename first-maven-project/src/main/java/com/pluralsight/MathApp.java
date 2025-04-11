a53900193b8e3ea6e0f4467058cd0af977528130
Kevin Galeana <kevinbgaleana@gmail.com>
2025-04-11 14:14:25 -0400

    completed Mod3 Exercise 2 - Mathapp

---
+++ first-maven-project/src/main/java/com/pluralsight/MathApp.java
@@ -0,0 +1,58 @@
+package com.pluralsight;
+
+public class Math.App {
+    public static void main;(String [] args){
+        int bobSalary = 11;
+        int garySalary = 52;
+
+        int highestSalary = Math.max(bobSalary, garySalary);
+        System.out.println(highestSalary);
+    }
+
+// Question 1:
+// declare variables here
+// then code solution
+// then use System.out.println() to display results
+// ex: System.out.println("The answer is " + answer);
+// REPEAT FOR NEXT EXERCISE
+    }
+    int carPrice = 10300;
+    int truckPrice = 34000;
+    int lowestPrice = Math.min(carPrice, truckPrice);
+
+    System.out.println("the lowesr price is " + lowestPrice);
+
+    double radius = 7.25F;
+    double areaOfCircle = Math.pi * Math.pow(radiusOfCircle,2);
+    System.out.println ("the area of the circle" + areaOfCircle);
+
+    double startingNumber = 5;
+    double squareRoot = math.sqrt(startingNumber);
+
+    System.out.println("The square root is"+ squareRoot);
+
+    public class DistanceCalculater {
+        public static main(String[] args){
+            //cordinates of the two points
+            int x1 = 5, y1 =10;
+            int  x2 = 85, y2 = 50;
+
+            //calculate the distance using the distance formula
+            double distance =Math.Sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+
+            //display the results
+            System.out.println("the distance is ");
+
+
+            float setNumber = 3.8F;
+            float absoluteNumber = Math.abs (setNumber);
+            System.out.println ("The absolute number of" + setNummber = "is"+ absoluteNumber);
+
+            double random = Math.random();
+            System.out.println(random);
+        }
+    }
+
+
+
+
